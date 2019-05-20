63623b43cfc0ae040d0a0c5039f42d4fdcf4a0eb
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-20 18:23:21 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,69 @@ module.exports = {
   get,
 };
 
+
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement === 20){
+    const newItem = {
+      ...item
+    }
+    return newItem;
+  } else if(succeed){
+    const newItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement + 1
+    }
+    return newItem
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    const newItem ={
+      name: item.name,
+      durability:item.durability -5,
+      enhancement:item.enhancement
+    }
+    return newItem;
+  } else if(item.enhancement > 16) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement -1,
+    }
+    return newItem;
+  }else if(item.enhancement >= 15){
+    const newItem ={
+      name: item.name,
+      durability:item.durability -10,
+      enhancement:item.enhancement
+    }
+    return newItem
+  } 
+  else{ return item}
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = {
+    name : item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return newItem;
 }
 
 function get(item) {
   return { ...item };
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,96 @@
-const enhancer = require('./enhancer.js');
+const { repair, fail, get, succeed } = require('./enhancer.js');
 // test away!
+
+// let item = {
+//     name: 'thing1',
+//     durability: 15,
+//     enhancement:15,
+// }
+
+describe('enhancer.js', () => {
+
+    //repair(item)
+    describe('repair', () => {
+        it('back to 100', ()=> {
+            let item = {
+                name: 'thing1',
+                durability: 15,
+                enhancement:15,
+            }
+
+            expect(repair(item)).toEqual({
+                name: 'thing1',
+                durability:100,
+                enhancement: 15
+            })
+        })
+    })
+    //fail(item)
+    describe('to fail', () => {
+        it('decrease durability by 5, if enhancement less than 15', ()=>{
+            let item = {
+                name: 'thing1',
+                durability: 50,
+                enhancement: 14,
+            }
+            expect(fail(item)).toEqual({
+                name: 'thing1',
+                durability:45,
+                enhancement: 14
+            })
+        })
+        it('decrease durability by 10, if enhancement is 15 and more', () => {
+            let item = {
+                name: 'thing1',
+                durability: 50,
+                enhancement: 15,
+            }
+            expect(fail(item)).toEqual({
+                name: 'thing1',
+                durability: 40,
+                enhancement: 15
+            })
+        })
+        it('decrease enhancement by 1, if enhancement is greater than 16', () => {
+            let item = {
+                name: 'thing1',
+                durability: 50,
+                enhancement: 19,
+            }
+            expect(fail(item)).toEqual({
+                name: 'thing1',
+                durability: 50,
+                enhancement: 18
+            })
+        })
+    })
+
+    //succeed
+    describe('succeed', ()=> {
+        it('item enhancement increase by 1', () => {
+            let item = {
+                name: 'thing1',
+                durability: 50,
+                enhancement:15
+            }
+            expect(succeed(item)).toEqual({
+                name: 'thing1',
+                durability: 50,
+                enhancement:16
+            })
+        })
+        it('enhancement max is 20', () => {
+            let item = {
+                name: 'thing1',
+                durability: 50,
+                enhancement: 20
+            }
+            expect(succeed(item)).toEqual({
+                name: 'thing1',
+                durability:50,
+                enhancement:20
+            })
+        })
+    })
+})
+
